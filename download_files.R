5ab40c25a3bcbbaa0e82a59b1bd5deb6f192b670
Derek Corcoran <au687614@uni.au.dk>
2021-09-06 15:08:31 +0200

    Started script

---
+++ download_files.R
@@ -0,0 +1,51 @@
+library(sf)
+library(raster)
+library(tidyverse)
+library(rgdal)
+library(rgdal)
+
+#Carpeta de capas
+
+Table_Layers <- read_csv("envidatS3paths.txt", col_names = FALSE) %>% 
+  magrittr::set_names("Link") %>% 
+  mutate(model = str_remove_all(Link, "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/bio/CHELSA_bio_mon_")) %>% 
+  tidyr::separate(model, sep = "_", into = c("Model", "RCP", "Coso", "Coso2", "Bio", "Year", "Version")) %>% 
+  dplyr::select(Link, Model, RCP, Year, Bio) %>% 
+  dplyr::filter(RCP == "rcp85")
+
+
+
+
+Layers <- Table_Layers %>% 
+  mutate(Bio = as.numeric(Bio)) %>% 
+  dplyr::group_split(Model) %>% 
+  purrr::map(~arrange(.x, Bio)) %>% 
+  purrr::map(~mutate(.x, Bio = formatC(Bio, digits = 1, flag = "0", mode = "integer")))
+
+Poliogono <- getData(name = "GADM", country = "GRL", level =0)
+
+for(i in 1:length(Layers)){
+  STACK <- list()
+  for(j in 1:nrow(Layers[[i]])){
+    while (length(STACK) < j) {
+      try({download.file(Layers[[i]]$Link[j], destfile = paste0("Temp", Layers[[i]]$Bio[j], ".tif"))
+        STACK[[j]] <- raster(paste0("Temp", Layers[[i]]$Bio[j], ".tif")) %>%
+          crop(Poliogono) %>% magrittr::set_names(paste0("bio", Layers[[i]]$Bio[j]))})
+    }
+    message(paste(j, "of", 19))
+  }
+  
+  Mask <- fasterize::fasterize(st_as_sf(Poliogono), STACK[[1]])
+  
+  STACK <- STACK %>% reduce(stack)
+  
+  STACK <- STACK*Mask
+  
+  writeRaster(STACK, paste(unique(Layers[[i]]$Model), unique(Layers[[i]]$RCP),".tif", sep = "_"), overwrite=TRUE)
+  
+  To_erase <- list.files(pattern = "Temp", full.names = T)
+  
+  file.remove(To_erase)
+  print(paste(i, "of", length(Layers), "Ready!"))
+}
+
