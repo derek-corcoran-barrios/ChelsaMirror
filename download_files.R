52a879cd418f89c4eed720e9427e679ec899067b
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-09-06 15:40:10 +0200

    Testing through line 46

--- download_files.R
+++ download_files.R
@@ -10,36 +10,43 @@ Table_Layers <- read_csv("envidatS3paths.txt", col_names = FALSE) %>%
   magrittr::set_names("Link") %>% 
   mutate(model = str_remove_all(Link, "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/cmip5/2061-2080/bio/CHELSA_bio_mon_")) %>% 
   tidyr::separate(model, sep = "_", into = c("Model", "RCP", "Coso", "Coso2", "Bio", "Year", "Version")) %>% 
-  dplyr::select(Link, Model, RCP, Year, Bio) %>% 
-  dplyr::filter(RCP == "rcp85")
+  dplyr::select(Link, Model, RCP, Year, Bio)
 
+Years <- unique(Table_Layers$Year)
+
+dir.create(Years)
+
+RCPs <- unique(Table_Layers$RCP)
+
+Folders <- paste(Years, RCPs, sep = "/")
+
+
+for(i in 1:length(Folders)){
+  dir.create(Folders[i])
+}
 
 
 
 Layers <- Table_Layers %>% 
   mutate(Bio = as.numeric(Bio)) %>% 
-  dplyr::group_split(Model) %>% 
+  dplyr::group_split(Model, RCP, Year) %>% 
   purrr::map(~arrange(.x, Bio)) %>% 
   purrr::map(~mutate(.x, Bio = formatC(Bio, digits = 1, flag = "0", mode = "integer")))
 
-Poliogono <- getData(name = "GADM", country = "GRL", level =0)
 
 for(i in 1:length(Layers)){
   STACK <- list()
   for(j in 1:nrow(Layers[[i]])){
     while (length(STACK) < j) {
       try({download.file(Layers[[i]]$Link[j], destfile = paste0("Temp", Layers[[i]]$Bio[j], ".tif"))
-        STACK[[j]] <- raster(paste0("Temp", Layers[[i]]$Bio[j], ".tif")) %>%
-          crop(Poliogono) %>% magrittr::set_names(paste0("bio", Layers[[i]]$Bio[j]))})
+        STACK[[j]] <- raster(paste0("Temp", Layers[[i]]$Bio[j], ".tif")) %>% 
+          magrittr::set_names(paste0("bio", Layers[[i]]$Bio[j]))})
     }
     message(paste(j, "of", 19))
   }
   
-  Mask <- fasterize::fasterize(st_as_sf(Poliogono), STACK[[1]])
-  
+
   STACK <- STACK %>% reduce(stack)
-  
-  STACK <- STACK*Mask
   
   writeRaster(STACK, paste(unique(Layers[[i]]$Model), unique(Layers[[i]]$RCP),".tif", sep = "_"), overwrite=TRUE)
   
